d632c147cf64f107a8abe99029c621a15ff23bc2
KRISHNA-663 <krish7cbe@gmail.com>
2023-08-28 19:10:07 +0530

    changes

--- sample1/src/App.js
+++ sample1/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  function Namechange()
+  {
+    const name = ["Aravinth","Neke","Jegan"];
+    const int =Math.floor(Math.random()*3);
+    return name[int];
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <center> Hi my name is KRISHNA.
+      <p>My best friend is {Namechange()}</p>
+     </center>
     </div>
   );
 }
