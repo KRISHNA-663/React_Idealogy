e900d48fb2ce620316637c5b597de1c5206eb6f5
KRISHNA-663 <krish7cbe@gmail.com>
2023-08-30 07:09:38 +0530

    changes

--- sample1/src/Footer.js
+++ sample1/src/Footer.js
@@ -10,6 +10,9 @@ const Footer = () => {
   const handleClick = () => {
     console.log("You are added as a friend without name.");
   }
+  {/*const handleClick3 = (e) => {
+    console.log(e.target.innerText);   //Using an object for the event
+  }*/}
   const handleClick2 = (name) => {
     console.log(`You are added as a friend ${name}`);
   }
@@ -18,6 +21,7 @@ const Footer = () => {
     <p>My best friend is {Namechange()}</p>
     <button onClick={() => handleClick2('KRISHNA')}>Add me as a friend</button>
     <button onClick={handleClick}>Add me as a friend without naming</button>
+    {/*<button onClick={(e) => handleClick3(e)}>Target</button>*/}
     </div>
   )
 }
